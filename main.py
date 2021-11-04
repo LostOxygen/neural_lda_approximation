cc9712f59ec53930a139104f94331101097ff2d2
jonathan <jonathan.evertz@gmail.com>
2021-11-04 20:44:59 +0100

    added lda training implementation

--- main.py
+++ main.py
@@ -3,21 +3,52 @@
 #!/usr/bin/env python3
 import time
 import os
+import socket
 import datetime
 import argparse
 import torch
 import numpy as np
 from utils.words import get_word_list
+from utils.lda import train_lda
 
-def main(gpu: int, workers: int, num_topics: int, verbose: bool) -> None:
+torch.backends.cudnn.benchmark = True
+
+def main(gpu: int, num_workers: int, num_topics: int, verbose: bool) -> None:
     """main method"""
+    start = time.perf_counter()
+    # set device properly
+    if gpu == 0:
+        DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    if gpu == 1:
+        DEVICE = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+
+    # print a summary of the chosen arguments
+    print("\n\n\n"+"#"*50)
+    print("## " + str(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")))
+    print("## System: {} CPU cores with {} GPUs on {}".format(torch.get_num_threads(),
+                                                             torch.cuda.device_count(),
+                                                             socket.gethostname()
+                                                             ))
+    if DEVICE == 'cpu':
+        print("## Using: CPU with ID {}".format(DEVICE))
+    else:
+        print("## Using: {} with ID {}".format(torch.cuda.get_device_name(device=DEVICE), DEVICE))
+    print("## Using {} workers for LDA computation".format(num_workers))
+    print("## Num_topics: {}".format(num_topics))
+    print("#"*50)
+    print("\n\n\n")
+
     words_list = get_word_list()
-    print(words_list)
+    train_lda(num_workers, num_topics, words_list)
+
+    end = time.perf_counter()
+    duration = (np.round(end - start) / 60.) / 60.
+    print(f"Computation time: {duration:0.4f} hours")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu", "-g", help="GPU", type=int, default=0)
-    parser.add_argument("--workers", "-w", help="number of workers for lda",
+    parser.add_argument("--num_workers", "-w", help="number of workers for lda",
                         type=int, default=4)
     parser.add_argument("--num_topics", "-t", help="number of topics for lda",
                         type=int, default=20)
--- utils/lda.py
+++ utils/lda.py
@@ -1,6 +1,27 @@
 """helper module to prepare and train the lda model"""
+import os
+import gensim
+from gensim.models import LdaMulticore
 
+LDA_PATH = "./models/"
 
-def train_lda(workers, topics, words_list) -> None:
-    """helper method to train and save a lda model"""
-    pass
+def train_lda(num_workers: int, num_topics: int, words_list: list) -> None:
+    """helper method to train and save a lda model in a specified path
+       :param num_workers: number of workers to compute the lda
+       :param num_topics: number of topics for the lda
+       :param words_list: the list of words on which the lda should be computed
+    """
+
+    dictionary = gensim.corpora.Dictionary(words_list)
+    bow_list = list(map(lambda x: dictionary.doc2bow(x), words_list))
+    ldamodel = LdaMulticore(bow_list, num_topics=num_topics, id2word=dictionary, passes=100, workers=num_workers)
+
+    print("[ saving lda model in {} ]".format(LDA_PATH+"lda_model"))
+    if not os.path.isdir(LDA_PATH):
+        os.mkdir(LDA_PATH)
+
+    try:
+        ldamodel.save(LDA_PATH+"lda_model")
+    except Exception as exception_lda:
+        print("[ could not save the lda model ]")
+        print(exception_lda)
