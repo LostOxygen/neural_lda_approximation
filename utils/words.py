41ebf6ef9a819e8c949ac139648b6f8644096026
jonathan <jonathan.evertz@gmail.com>
2021-11-18 11:19:09 +0100

    fixed sparse vector length

--- utils/words.py
+++ utils/words.py
@@ -59,7 +59,8 @@ def save_train_data() -> None:
             sparse_indizes.append(int(key))
             sparse_inputs.append(float(val))
 
-        input_d = torch.sparse_coo_tensor(torch.tensor(sparse_indizes), torch.tensor(sparse_inputs), [len(dictionary)])
+        input_d = torch.sparse_coo_tensor(torch.tensor(sparse_indizes), torch.tensor(sparse_inputs),
+                                          (1, len(dictionary)))
 
         # write everything as python pickles into a tar file
         sink.write({
